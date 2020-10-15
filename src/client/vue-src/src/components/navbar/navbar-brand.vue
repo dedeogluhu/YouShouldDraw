e3dc445ee6281e288adb12cb592f1f0f32f47e01
Hüseyin Dedeoğlu <64230499+dedeogluhu@users.noreply.github.com>
2020-10-15 12:20:38 +0300

    Add sidebar

--- src/client/vue-src/src/components/navbar/navbar-brand.vue
+++ src/client/vue-src/src/components/navbar/navbar-brand.vue
@@ -1,7 +1,32 @@
 <template>
-  <b-navbar-brand href="#">
+  <b-navbar-brand
+    href="#"
+    v-b-toggle.sidebar-1
+    v-b-tooltip.hover
+    title="Click me to open the sidebar"
+  >
     You Should Draw
     <b-icon icon="brush"></b-icon>
+
+    <b-sidebar
+      id="sidebar-1"
+      shadow
+      bg-variant="dark"
+      text-variant="light"
+      no-header
+    >
+      <div class="px-3 py-2">
+        <h3 class="text-light text-center mt-3">
+          You Should Draw <b-icon icon="brush"></b-icon>
+        </h3>
+        <hr />
+        <b-button block variant="dark"> Background </b-button>
+        <br />
+        <b-button block variant="dark"> About </b-button>
+        <br />
+        <b-button block variant="dark"> Help </b-button>
+      </div>
+    </b-sidebar>
   </b-navbar-brand>
 </template>
 
