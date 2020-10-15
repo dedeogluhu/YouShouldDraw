e8d4302f0efb5cf88ac60c6b194272b2570ed76f
Hüseyin Dedeoğlu <64230499+dedeogluhu@users.noreply.github.com>
2020-10-15 16:28:58 +0300

    Add about modal component

---
+++ src/client/vue-src/src/components/navbar/navbar-sidebar/about-modal.vue
@@ -0,0 +1,19 @@
+<template>
+  <b-modal id="about-modal" title="About" ok-only>
+    <p class="my-4">
+      Hi, I am a web developer who always tries to make and contribute to open
+      source projects. When i learned about the canvas api, i tried to make a
+      project. So I started to research more about it. And the project looked
+      nice so wanted to make it public.
+    </p>
+    You can find the source here :
+    <a href="https://github.com/dedeogluhu/YouShouldDraw">Github</a>
+  </b-modal>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
