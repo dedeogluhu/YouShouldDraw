5e9343c2073493eb9fdfe67609e202e260d0caa4
Hüseyin Dedeoğlu <64230499+dedeogluhu@users.noreply.github.com>
2020-10-17 10:18:01 +0300

    Help modal footer styling

--- src/client/vue-src/src/components/navbar/navbar-sidebar/help-modal.vue
+++ src/client/vue-src/src/components/navbar/navbar-sidebar/help-modal.vue
@@ -1,9 +1,12 @@
 <template>
-  <b-modal id="help-modal" title="Help" ok-only>
+  <b-modal id="help-modal" title="Help" ok-only hide-footer>
     <p>
       I think the app is easy to use but wanted to add this section in case I
       add things difficult to understand
     </p>
+    <b-button class="mt-3" block @click="$bvModal.hide('help-modal')">
+      Ok
+    </b-button>
   </b-modal>
 </template>
 
