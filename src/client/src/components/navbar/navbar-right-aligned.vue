4940666632dada4aefb6f3551e6a72815237a4a1
Hüseyin Dedeoğlu <64230499+dedeogluhu@users.noreply.github.com>
2020-11-01 15:29:23 +0300

    Code cleanup

--- src/client/src/components/navbar/navbar-right-aligned.vue
+++ src/client/src/components/navbar/navbar-right-aligned.vue
@@ -1,8 +1,8 @@
 <template>
   <b-navbar-nav class="ml-auto">
     <b-nav-form>
-      <b-nav-text v-if="suggestion" class="mr-3 text-light"
-        >You Should Draw {{ suggestion }}
+      <b-nav-text v-if="suggestion" class="mr-3 text-light">
+        You Should Draw {{ suggestion }}
       </b-nav-text>
       <b-button
         class="btn btn-outline-success my-2 my-sm-0 btn-dark"
